001e7a73adabf9a9a9fc7770556575401a849bbf
thaiclk <thaiclk.2710@gmail.com>
2026-04-21 11:52:21 +0700

    fix: location

--- webapp/controller/SiteDetail.controller.js
+++ webapp/controller/SiteDetail.controller.js
@@ -2070,31 +2070,7 @@ sap.ui.define([
                             bLocationError = true;
                         }
 
-                        // 2. POS fields: mandatory
-                        if (!sLStartVal) {
-                            oLocStart.setValueState("Error");
-                            oLocStart.setValueStateText(oBundle.getText("posStartRequired"));
-                            bHasError = true;
-                            bLocationError = true;
-                        }
-                        if (!sLEndVal) {
-                            oLocEnd.setValueState("Error");
-                            oLocEnd.setValueStateText(oBundle.getText("posEndRequired"));
-                            bHasError = true;
-                            bLocationError = true;
-                        }
-                        if (!sLBotVal) {
-                            oLocBot.setValueState("Error");
-                            oLocBot.setValueStateText(oBundle.getText("posBotRequired"));
-                            bHasError = true;
-                            bLocationError = true;
-                        }
-                        if (!sLTopVal) {
-                            oLocTop.setValueState("Error");
-                            oLocTop.setValueStateText(oBundle.getText("posTopRequired"));
-                            bHasError = true;
-                            bLocationError = true;
-                        }
+
 
                         // 3. POS_START <= POS_END
                         if (!isNaN(fLStart) && !isNaN(fLEnd) && fLStart > fLEnd) {
